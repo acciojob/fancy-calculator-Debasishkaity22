250be0db5d1cc27591c5ae058d35d0172266dc6d
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-06 11:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let first=promt("enter the first input");
+let result=first*2;
+let second=promt("enter the second input");
+result+=second;
+alert("The final result is :"+result);
